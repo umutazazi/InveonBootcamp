7d8d064216e56db48803c885aac5fe7620c7faad
Umut Azazi <72599457+umutazazi@users.noreply.github.com>
2024-12-09 20:06:23 +0300

    deleted

--- Desktop/mdp group project/mdp-spotify/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import searchReducer from "./searchSlice";
-
-export const store = configureStore({
-  reducer: {
-    search: searchReducer,
-  },
-});
